3db12698d7b310e80327080f3d49eef514244406
Cong Gao @ CEE-MacBooK16 <cnggao@gmail.com>
2026-07-24 19:40:19 -0400

    update the single_TC files

--- unzip_files.sh
+++ unzip_files.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-# extract every .zip in the zip directory into that singleTC folder
+# extract every .zip in the zip directory, flattening all files into singleTC
 for z in zip/*.zip; do
     echo $z
-    unzip -q "$z" -d singleTC
+    unzip -joq "$z" -d singleTC
 done
+
+# Normalize all extracted file permissions.
+find singleTC -type f -exec chmod 644 {} +
